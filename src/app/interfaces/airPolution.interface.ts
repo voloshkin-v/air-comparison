b5ee9ea45fbdbc3e701310a3d8a38c6f2f457cd8
Vitalii <voloshin.v.02@gmail.com>
2024-04-10 14:27:48 +0200

    add method for fetching air polution data

---
+++ src/app/interfaces/airPolution.interface.ts
@@ -0,0 +1,24 @@
+export interface AirPolution {
+    coord: {
+        lon: number;
+        lat: number;
+    };
+    list: [
+        {
+            main: {
+                aqi: number;
+            };
+            components: {
+                co: number;
+                no: number;
+                no2: number;
+                o3: number;
+                so2: number;
+                pm2_5: number;
+                pm10: number;
+                nh3: number;
+            };
+            dt: number;
+        }
+    ];
+}
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from './../../environments/environment';
 import { Geocoding } from '../interfaces/geocoding.interface';
+import { AirPolution } from '../interfaces/airPolution.interface';
 
 @Injectable({
     providedIn: 'root',
@@ -12,6 +13,12 @@ export class WeatherService {
     getGeocoding(location: string, limit = 5) {
         return this.http.get<Geocoding[]>(
             `${environment.apiBaseUrl}/geo/1.0/direct?q=${location}&limit=${limit}&appid=${environment.apiKey}`
+        );
+    }
+
+    getAirPolutionData(lat: number, lon: number) {
+        return this.http.get<AirPolution>(
+            `${environment.apiBaseUrl}/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${environment.apiKey}`
         );
     }
 }
